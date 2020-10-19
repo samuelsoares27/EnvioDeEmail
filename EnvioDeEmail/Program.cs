627f6177b1c9db384ac2aa0359af1d811621b2bf
Samuel Soares <34168199+samuelsoares27@users.noreply.github.com>
2020-10-19 20:22:02 -0300

    Add project files.

---
+++ EnvioDeEmail/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace EnvioDeEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar o Email
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("Remetente");
+            mail.To.Add(new MailAddress("Destinatário"));
+            mail.Subject = "Ola mundo title!";
+            mail.Body = " Ola mundo body! ";
+
+            using (var smtp = new System.Net.Mail.SmtpClient())
+            {
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("Email Remetente", "Senha Remetente");
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+            }
+
+        }
+    }
+}
